4f2d6d0ee6d53ab5641ed4144c978d64488e41e7
Brad Searle <bradleysearle@gmail.com>
2020-09-13 20:39:59 +0000

    get requests

--- main.go
+++ main.go
@@ -24,15 +24,27 @@ func main() {
 
 	server := serverConfig()
 	cookie := server.auth()
-	response := server.getStatus(cookie)
+
+	// List Users
+	// response := server.getRequest(fmt.Sprintf(`https://%s/api/users/`, server.Server), cookie)
+
+	// List Folders
+	// Not working
+	// response := server.getRequest(fmt.Sprintf(`https://%s/api/labs`, server.Server), cookie)
+
+	// List Roles
+	response := server.getRequest(fmt.Sprintf(`https://%s/api/list/roles`, server.Server), cookie)
 
 	defer response.Body.Close()
 	fmt.Println("response Status:", response.Status)
 	fmt.Println("response Headers:", response.Header)
 	fmt.Print("response Body: ")
+
 	io.Copy(os.Stdout, response.Body)
-	jsonData := eveServerToJSON(server)
-	jsonToFile("output.json", jsonData, 0600)
+	fmt.Println("")
+
+	// jsonData := eveServerToJSON(server)
+	// jsonToFile("output.json", jsonData, 0600)
 }
 
 func serverConfig() eveServer {
@@ -117,6 +129,64 @@ func (es eveServer) getStatus(cookie http.Cookie) *http.Response {
 	data, _ := json.Marshal(map[string]string{})
 
 	req, err := http.NewRequest("GET", fmt.Sprintf(`https://%s/api/status`, es.Server), bytes.NewBuffer(data))
+	if err != nil {
+		log.Fatal("Error reading request. ", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+
+	req.AddCookie(&cookie)
+
+	if err != nil {
+		log.Fatal("Error reading body. ", err)
+	}
+
+	// Set client timeout
+	client := &http.Client{Timeout: time.Second * 10}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+
+	return resp
+
+}
+
+func (es eveServer) getFolders(cookie http.Cookie) *http.Response {
+	data, _ := json.Marshal(map[string]string{})
+
+	req, err := http.NewRequest("GET", fmt.Sprintf(`https://%s/api/folders`, es.Server), bytes.NewBuffer(data))
+	if err != nil {
+		log.Fatal("Error reading request. ", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+
+	req.AddCookie(&cookie)
+
+	if err != nil {
+		log.Fatal("Error reading body. ", err)
+	}
+
+	// Set client timeout
+	client := &http.Client{Timeout: time.Second * 10}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error reading response. ", err)
+	}
+
+	return resp
+
+}
+
+func (es eveServer) getRequest(url string, cookie http.Cookie) *http.Response {
+	data, _ := json.Marshal(map[string]string{})
+
+	req, err := http.NewRequest("GET", url, bytes.NewBuffer(data))
 	if err != nil {
 		log.Fatal("Error reading request. ", err)
 	}
